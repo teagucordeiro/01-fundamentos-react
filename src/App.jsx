bdd88bb5c4cd08c91c0e2284a867cfb4cc41bc1e
Thiago Cordeiro <thiagoty52@gmail.com>
2024-06-20 19:35:47 -0300

    :sparkles: feat: create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Thiago Cordeiro"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quas, illo officia laudantium cum earum, impedit, aspernatur in sequi atque vel provident vitae tempore eius delectus optio iste consequuntur iusto?"
