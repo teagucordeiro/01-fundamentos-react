945b15f4a040638a61194bcf9251a3cdd6dfa56e
Thiago Cordeiro <thiagoty52@gmail.com>
2024-06-25 17:13:22 -0300

    :sparkles: feat: create sidebar  component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from './App.module.css';
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Thiago Cordeiro"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quas, illo officia laudantium cum earum, impedit, aspernatur in sequi atque vel provident vitae tempore eius delectus optio iste consequuntur iusto?"
-      />
-      <Post 
-        author="Elon Musk"
-        content="Conteúdo legal"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post
+          author="Thiago Cordeiro"
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quas, illo officia laudantium cum earum, impedit, aspernatur in sequi atque vel provident vitae tempore eius delectus optio iste consequuntur iusto?"
+        />
+        <Post author="Elon Musk" content="Conteúdo legal" />
+        </main>
+      </div>
     </div>
   );
 }
