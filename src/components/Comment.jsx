54efc0b8b76f8100a02b4ceefd2a8c55c87bad78
Thiago Cordeiro <thiagoty52@gmail.com>
2024-08-20 10:20:40 -0300

    :sparkles: feat: create comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,39 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/teagucordeiro.png" alt="" />
+      
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Thiago Cordeiro</strong>
+              <time
+                dateTime="2024-06-29 22:00:00"
+                title="29 de Junho às 22:06h"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
