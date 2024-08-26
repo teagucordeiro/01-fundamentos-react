7165bf99da92a04764b07d4cce96ded6e0232a3b
Thiago Cordeiro <thiagoty52@gmail.com>
2024-08-26 16:10:57 -0300

    :sparkles: feat: create posts fake content

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,57 @@ import styles from "./App.module.css";
 
 import "./global.css";
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      authorUrl: "https://github.com/teagucordeiro.png",
+      name: "Thiago Cordeiro",
+      role: "CTO @Techay",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2024-08-26 16:00:00"),
+  },
+  {
+    id: 2,
+    author: {
+      authorUrl: "https://github.com/maykbrito.png",
+      name: "Mayk Brito",
+      role: "Educator @Rockseat",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    publishedAt: new Date("2024-08-28 16:00:00"),
+  },
+];
+
 export function App() {
   return (
     <div>
@@ -13,8 +64,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map((post) => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
         </main>
       </div>
     </div>
